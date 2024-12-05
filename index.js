0e3579b68f2819f0136577f5ce307e26a2161770
bnanim <bernardanim86@gmail.com>
2024-12-05 12:38:03 +0000

    new file:   images/building.JPG 	new file:   images/robot.jpeg 	new file:   images/robot.jpg 	new file:   index.css 	new file:   index.html 	new file:   my-logo.png

---
+++ index.js
@@ -0,0 +1,5 @@
+const burgerEl = document.querySelector('.burger');
+const offScreenMenuEl = document.querySelector('.off-screen-menu');
+burgerEl.addEventListener('click', function () {
+    offScreenMenuEl.classList.toggle('active');
+}) 
