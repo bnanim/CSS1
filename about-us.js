1e9db51a5733111ebd81a966c3f3b4ab647a3dec
bnanim <bernardanim86@gmail.com>
2024-12-09 09:00:33 +0000

    new file:   .gitignore 	modified:   about-us.css 	modified:   about-us.html 	modified:   about-us.js 	modified:   index.css

--- about-us.js
+++ about-us.js
@@ -3,4 +3,18 @@ const offScreenMenuEl = document.querySelector('.off-screen-menu');
 burgerEl.addEventListener('click', function () {
     offScreenMenuEl.classList.toggle('active');
     burgerEl.classList.toggle('active');
-}) 
+})
+const hiddenEl = document.querySelector('.hidden');
+const vmButtonEl = document.querySelector('.view-more');
+const vlButtonEl = document.querySelector('.view-less');
+
+vmButtonEl.addEventListener('click', function () {
+    vmButtonEl.classList.toggle('active');
+    hiddenEl.classList.toggle('active');
+    vlButtonEl.classList.toggle('active');
+})
+vlButtonEl.addEventListener('click', function () {
+    vmButtonEl.classList.toggle('active');
+    hiddenEl.classList.toggle('active');
+    vlButtonEl.classList.toggle('active');
+})
